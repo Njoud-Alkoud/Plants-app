4d34b67430eb31b78695120777eb9934a5276fcb
Njoud Mohammed alkoud <nalkoudmm@gmail.com>
2025-10-27 15:23:08 +0300

    adding the custmize plants

---
+++ Plants app/GlassModifier.swift
@@ -0,0 +1,8 @@
+//
+//  GlassModifier.swift
+//  Plants app
+//
+//  Created by Njoud Mohammed Alkoud on 01/05/1447 AH.
+//
+
+import Foundation
---
+++ Plants app/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  Plants app
+//
+//  Created by Njoud Mohammed Alkoud on 30/04/1447 AH.
+//
+
+import Foundation
